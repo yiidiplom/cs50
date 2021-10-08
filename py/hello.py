26f328d16a3130d589e4f0d4ce8cbdf23f104c32
yiidiplom <>
2021-10-08 18:54:21 +0300

    py+

--- py/hello.py
+++ py/hello.py
@@ -1,1 +1,19 @@
-print ("hello")
+#print ("hello")
+#name = input("Name: ")
+#print("Hello, "+ name+"!")
+#print(f"Hello {name}!")
+# n = int(input("Number: "))
+# if n>0:
+	# print (f"{n} - is positive")
+# elif n<0:
+	# print ("n - is negative")
+# else:
+	# print ("n - is zero")
+
+class Point():
+	def __init__(self,x1,y1):
+		self.x = x1
+		self.y = y1
+
+p=Point(5,7)
+print(f"x={p.x}  y={p.y}")
